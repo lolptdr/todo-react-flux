26cfa8f85cf0b7c3c23161e2c406b4d108e697d3
lolptdr <jtingsan@gmail.com>
2015-11-24 16:39:22 -0600

    Add babel presets, fix badly spelt methods, now working Todo

---
+++ js/components/Header.react.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import TodoTextInput from './TodoTextInput.react'
+
+class Header extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+
+  onSave(text) {
+    console.log(text)
+  }
+
+  render() {
+    const pHolder = "Todos"
+    return (
+      <div>
+        <h2>Todo List</h2>
+        <TodoTextInput onSave={this.onSave.bind(this)} placeholder={pHolder} />
+      </div>
+    )
+  }
+}
+
+export default Header
---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+var path = require("path");
+module.exports = {
+
+// import path from 'path'
+// export default {
+  entry:['./js/app.js'
+  ],
+
+  output: {
+    filename: 'bundle.js',
+    path: path.join(__dirname, 'build'),
+    publicPath: 'http://localhost:8080/'
+  },
+
+  module: {
+    loaders: [{
+      test: /\.js$/,
+      exclude: /node_modules/,
+      loaders: ['react-hot', 'babel']
+    }],
+  },
+
+}
